1700745dfe057f77708e8150a15919623b3aed39
george-bits-coder <91771684+george-bits-coder@users.noreply.github.com>
2022-01-05 00:16:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,14 +7,34 @@ import Displaystatus from "./displaystatus";
 
 export default function App() {
   const [isAuth, setIsAuth] = useState(false);
-
+  const [token, setToken] = useState("");
   const toggleau = () => {
     if (isAuth) setIsAuth(false);
     else setIsAuth(true);
   };
+
+  if (isAuth) {
+    const payload = {
+      email: "eve.holt@reqres.in",
+      password: "cityslicka"
+    };
+
+    fetch("https://reqres.in/api/login", {
+      method: "POST",
+      body: JSON.stringify(payload),
+      headers: {
+        "content-type": "application/json"
+      }
+    })
+      .then((d) => d.json())
+      .then((res) => {
+        setToken(res.token);
+      });
+  }
+
   return (
     <div className="App">
-      <AuthContext.Provider value={{ isAuth, setIsAuth, toggleau }}>
+      <AuthContext.Provider value={{ isAuth, setIsAuth, toggleau, token }}>
         {<Navbar />}
         <Displaystatus />
       </AuthContext.Provider>
