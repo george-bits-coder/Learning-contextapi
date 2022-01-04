dfad92c089886851f31900244ab782a73a384282
george-bits-coder <91771684+george-bits-coder@users.noreply.github.com>
2022-01-05 00:17:02 +0530

    Update displaystatus.js

--- src/displaystatus.js
+++ src/displaystatus.js
@@ -3,11 +3,11 @@ import { useContext } from "react";
 import { AuthContext } from "./Contexts/AuthContext";
 
 export default function Displaystatus() {
-  const { isAuth, setIsAuth } = useContext(AuthContext);
+  const { isAuth, setIsAuth, token } = useContext(AuthContext);
 
   return (
     <>
-      <div>{isAuth ? "Loggedin" : "Not Loggedin"}</div>
+      <div>{isAuth ? `Loggedin with token ${token}` : "Not Loggedin"}</div>
     </>
   );
 }
